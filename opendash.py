5a24899316de3e9a0b98ac074cfb2bea9d3112a2
Unai Aguilera <unai.aguilera@deusto.es>
2014-01-14 17:16:08 +0100

    Classes obtained from server

--- opendash.py
+++ opendash.py
@@ -3,90 +3,111 @@
 from flask import Flask, render_template
 from flask import jsonify, request
 
+import rdflib
+
 import random
 
 app = Flask(__name__)
 
+endpoints = [
+	{	'id': 0,
+		'url': 'http://helheim.deusto.es/sparql'
+	}, 
+	{	'id': 1,
+		'url': 'http://test2'
+	}, 
+	{	'id': 2,
+		'url': 'http://test3'
+	}
+]
+
 @app.route("/report")
 def show_chart():
-    return render_template('report.html')
+	return render_template('report.html')
 
 @app.route("/endpoints")
 def get_endpoints():
-	endpoints = [
-				{	'id': 0,
-				 	'url': 'http://test1'
-				}, 
-				{	'id': 1,
-					'url': 'http://test2'
-				}, 
-				{	'id': 2,
-					'url': 'http://test3'
-				}
-				]
-
 	return jsonify(endpoints=endpoints)
 
-@app.route("/endpoints/<id>/graphs")
-def get_endpoint_graphs(id):
-	if id == '0':
-		graphs = 	[
-					{	'id': 0,
-					  	'name': 'http://test1/graph1'
-					}, 
-					{ 	'id': 1,
-						'name': 'http://test1/graph2'
-					}
-					]
+@app.route("/endpoints/get_graphs", methods=['POST'])
+def get_endpoint_graphs():
+	endpoint = request.form['endpoint']
 
-		return jsonify(graphs=graphs)
-	else:
-		return jsonify(graphs=[])
+	g = rdflib.ConjunctiveGraph('SPARQLStore')
+	g.open(endpoint)
 
-@app.route("/endpoints/<endpointID>/graphs/<graphID>")
-def get_source_description(endpointID, graphID):
-	if endpointID == '0' and graphID == '0':
-		desc = 	{ 	'endpointID': endpointID,
-					'graphID': graphID,
-					'classes': [
-					{ 	'classURI': 'http://test1/class1',
-						'properties': 	[
-										{ 	'uri': 'http://test1/property1',
-											'datatype': 'http://test1/type1'
-										},
-										{ 	'uri': 'http://test1/property2',
-											'datatype': 'http://test1/type2'
-										}		
-										]
-					},
-					{ 	'classURI': 'http://test1/class2',
-						'properties': 	[
-										{ 	'uri': 'http://test1/property3',
-											'datatype': 'http://test1/type1'
-										},
-										{ 	'uri': 'http://test1/property4',
-											'datatype': 'http://test1/type2'
-										}		
-										]
-					}]
-				}
+	qres = g.query("select distinct ?g where { graph ?g {?s ?p ?o} }")
 
-		return jsonify(desc=desc)
-	else:
-		return jsonify(desc=[])
+	graphs = []
+	for index, graph in enumerate(qres):
+		graphs.append({'id': index, 'name': graph[0]})
 
-@app.route("/endpoints/<endpointID>/graphs/<graphID>/data")
-def get_data(endpointID, graphID):
-	if endpointID == '0' and graphID == '0':
-		data = 	[	random.randrange(1000), 
-					random.randrange(1000),
-					random.randrange(1000),
-					random.randrange(1000)
-				]
+	return jsonify(graphs=graphs)
 
-		return jsonify(data=data)
-	else:
-		return jsonify(data=data)
+ignored = []
+
+def loadIgnoredPrefixes():
+	with open('ignored.txt') as f:
+		ignored = f.read().splitlines()
+
+def getFilter(var):
+	filter = ''
+	if len(ignored) > 0:
+		filter += "FILTER ("
+
+		first = True
+		for prefix in ignored:
+			if first:
+				filter += " !regex(str(%s), \"%s\")" % (var, prefix)
+				first = False
+			else:
+				filter += " && !regex(str(%s), \"%s\")" % (var, prefix)
+		filter += ")"
+
+	return filter
+
+@app.route("/endpoints/get_description", methods=['POST'])
+def get_source_description():
+	endpoint = request.form['endpoint']
+	graph = request.form['graph']
+
+	g = rdflib.ConjunctiveGraph('SPARQLStore')
+	g.open(endpoint)
+
+	query = "SELECT DISTINCT ?class FROM <%s> WHERE { [] a ?class " + getFilter("?class") + " } LIMIT 50"
+	qres = g.query(query %graph)
+
+	desc = {}
+	desc['endpoint'] = endpoint
+	desc['graph'] = graph
+
+	classes = []
+	for c in qres:
+		clazz = {}
+		clazz['classURI'] = c[0]
+
+		clazz['properties'] = 	[{ 	'uri': 'http://test1/property1',
+									'datatype': 'http://test1/type1'
+								},
+								{ 	'uri': 'http://test1/property2',
+									'datatype': 'http://test1/type2'
+								}]
+		classes.append(clazz)
+
+	desc['classes'] = classes
+
+	return jsonify(desc=desc)
+
+@app.route("/endpoints/get_data", methods=['POST'])
+def get_data():
+	data = 	[	random.randrange(1000), 
+				random.randrange(1000),
+				random.randrange(1000),
+				random.randrange(1000)
+			]
+	return jsonify(data=data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	loadIgnoredPrefixes()
+
+	app.run(debug=True)
